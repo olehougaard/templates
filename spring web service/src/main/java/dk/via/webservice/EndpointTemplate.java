6e93e56242a4ebbfe87d00c3acf73e8852f29f12
= <=>
2020-09-28 10:59:13 +0200

    Clean up after testing template

--- spring web service/src/main/java/dk/via/webservice/EndpointTemplate.java
+++ spring web service/src/main/java/dk/via/webservice/EndpointTemplate.java
@@ -1,19 +1,17 @@
 package dk.via.webservice;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
 import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
 import org.springframework.ws.server.endpoint.annotation.RequestPayload;
 import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
 
-import io.spring.guides.gs_producing_web_service.GetCountryRequest;
-import io.spring.guides.gs_producing_web_service.GetCountryResponse;
-
 @Endpoint
 public class EndpointTemplate {
-    @PayloadRoot(namespace = WebServiceConfig.NAMESPACE_URI, localPart = "getCountryRequest")
+    /*
+    @PayloadRoot(namespace = WebServiceConfig.NAMESPACE_URI, localPart = "method")
     @ResponsePayload
     public MethodResponse method(@RequestPayload MethodRequest request) {
         return null;
     }
+     */
 }
--- spring web service/src/main/java/dk/via/webservice/WebServiceConfig.java
+++ spring web service/src/main/java/dk/via/webservice/WebServiceConfig.java
@@ -18,17 +18,17 @@ public class WebServiceConfig extends WsConfigurerAdapter {
     public static final String NAMESPACE_URI = "http://via.dk/web-service";
 
     @Bean
-    public ServletRegistrationBean messageDispatcherServlet(ApplicationContext applicationContext) {
+    public ServletRegistrationBean<MessageDispatcherServlet> messageDispatcherServlet(ApplicationContext applicationContext) {
         MessageDispatcherServlet servlet = new MessageDispatcherServlet();
         servlet.setApplicationContext(applicationContext);
         servlet.setTransformWsdlLocations(true);
-        return new ServletRegistrationBean(servlet, "/ws/*");
+        return new ServletRegistrationBean<>(servlet, "/ws/*");
     }
 
-    @Bean(name = "countries")
+    @Bean(name = "webservice")
     public DefaultWsdl11Definition defaultWsdl11Definition(XsdSchema countriesSchema) {
         DefaultWsdl11Definition wsdl11Definition = new DefaultWsdl11Definition();
-        wsdl11Definition.setPortTypeName("CountriesPort");
+        wsdl11Definition.setPortTypeName("WebservicePort");
         wsdl11Definition.setLocationUri("/ws");
         wsdl11Definition.setTargetNamespace(NAMESPACE_URI);
         wsdl11Definition.setSchema(countriesSchema);
